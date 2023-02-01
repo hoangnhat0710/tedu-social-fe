c982d1a0fc86caa5867875c2f280dd1e8455d02b
tomosiahoang <nhat.hoang@tomosia.com>
2023-02-01 22:05:10 +0700

    user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,1 +1,26 @@
 import env from 'react-dotenv';
+
+const login = (email: string, password: string) => {
+     const request = {
+        methods: 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({email, password})
+     }
+
+     return fetch(`${env.API_URL}/api/v1/auth`, request)
+     .then((res) => {
+        sessionStorage.setItem('user', JSON.stringify(res));
+        return res;
+     });
+}
+
+const logout = () => {
+     sessionStorage.removeItem('user');
+}
+
+export const userService = {
+    login,
+    logout
+}
